25baed04d69eb4783e195bb2e36be2f0c6540a62
Daniel <danya.ostap@gmail.com>
2023-11-17 11:22:28 +0200

    Add Base Point getter for `ECWrapper`

--- pkg/elliptic/ecwrapper.go
+++ pkg/elliptic/ecwrapper.go
@@ -6,6 +6,18 @@ type ECWrapper struct {
 	curve elliptic.Curve
 }
 
+func NewECWrapper(curve elliptic.Curve) *ECWrapper {
+	return &ECWrapper{
+		curve: curve,
+	}
+}
+
 func (ec *ECWrapper) Params() *elliptic.CurveParams {
 	return ec.curve.Params()
+}
+
+func (ec *ECWrapper) GetBasePointG() *ECPoint {
+	params := ec.Params()
+	Gx, Gy := params.Gx, params.Gy
+	return NewECPoint(Gx, Gy)
 }
