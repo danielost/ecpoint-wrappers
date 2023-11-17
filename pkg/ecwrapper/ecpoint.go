1bc1edf4e5c61079c902cf01f1896b6649c76032
Daniel <danya.ostap@gmail.com>
2023-11-17 11:51:42 +0200

    Add serialization methods to `ECPoint`

--- pkg/ecwrapper/ecpoint.go
+++ pkg/ecwrapper/ecpoint.go
@@ -3,6 +3,7 @@ package ecwrapper
 import (
 	"fmt"
 	"math/big"
+	"strings"
 )
 
 type ECPoint struct {
@@ -22,4 +23,30 @@ func (ecp *ECPoint) Params() (*big.Int, *big.Int) {
 
 func (ecp *ECPoint) Print(base int) {
 	fmt.Printf("X:%s\nY:%s\n", ecp.x.Text(base), ecp.y.Text(base))
+}
+
+func ECPointToString(point *ECPoint, base int) string {
+	x, y := point.Params()
+	return x.Text(base) + ":" + y.Text(base)
+}
+
+func StringToECPoint(s string, base int) (*ECPoint, error) {
+	params := strings.Split(s, ":")
+	if len(params) != 2 {
+		return nil, fmt.Errorf("wrong number of parameters expected=%d got=%d", 2, len(params))
+	}
+
+	var x, y *big.Int
+
+	x, ok := x.SetString(params[0], base)
+	if !ok {
+		return nil, fmt.Errorf("invalid value for x")
+	}
+
+	y, ok = y.SetString(params[1], base)
+	if !ok {
+		return nil, fmt.Errorf("invalid value for y")
+	}
+
+	return NewECPoint(x, y), nil
 }
