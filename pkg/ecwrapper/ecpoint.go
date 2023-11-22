ccc4a4123966df4b6b542207d1991efa62670dd5
Daniel <danya.ostap@gmail.com>
2023-11-22 11:11:49 +0200

    Add comments

--- pkg/ecwrapper/ecpoint.go
+++ pkg/ecwrapper/ecpoint.go
@@ -6,10 +6,12 @@ import (
 	"strings"
 )
 
+// ECPoint represents a point on an elliptic curve with coordinates (x, y).
 type ECPoint struct {
 	x, y *big.Int
 }
 
+// NewECPoint creates a new ECPoint with the given x and y coordinates.
 func NewECPoint(x, y *big.Int) *ECPoint {
 	return &ECPoint{
 		x: x,
@@ -17,25 +19,30 @@ func NewECPoint(x, y *big.Int) *ECPoint {
 	}
 }
 
+// Params returns the x and y coordinates of the ECPoint.
 func (ecp *ECPoint) Params() (*big.Int, *big.Int) {
 	return ecp.x, ecp.y
 }
 
+// Print prints the x and y coordinates of the ECPoint in the specified base.
 func (ecp *ECPoint) Print(base int) {
 	fmt.Printf("X:%s\nY:%s\n", ecp.x.Text(base), ecp.y.Text(base))
 }
 
+// IsEqual checks if two ECPoints are equal by comparing their x and y coordinates.
 func (ecp *ECPoint) IsEqual(other *ECPoint) bool {
 	x1, y1 := ecp.Params()
 	x2, y2 := other.Params()
 	return x1.Cmp(x2) == 0 && y1.Cmp(y2) == 0
 }
 
+// ECPointToString converts an ECPoint to a string representation in the specified base.
 func ECPointToString(point *ECPoint, base int) string {
 	x, y := point.Params()
 	return x.Text(base) + ":" + y.Text(base)
 }
 
+// StringToECPoint converts a string representation to an ECPoint in the specified base.
 func StringToECPoint(s string, base int) (*ECPoint, error) {
 	params := strings.Split(s, ":")
 	if len(params) != 2 {
@@ -44,11 +51,13 @@ func StringToECPoint(s string, base int) (*ECPoint, error) {
 
 	var x, y *big.Int
 
+	// Convert string values to big.Int for x coordinate.
 	x, ok := x.SetString(params[0], base)
 	if !ok {
 		return nil, fmt.Errorf("invalid value for x")
 	}
 
+	// Convert string values to big.Int for y coordinate.
 	y, ok = y.SetString(params[1], base)
 	if !ok {
 		return nil, fmt.Errorf("invalid value for y")
--- pkg/ecwrapper/ecwrapper.go
+++ pkg/ecwrapper/ecwrapper.go
@@ -5,31 +5,37 @@ import (
 	"math/big"
 )
 
+// ECWrapper wraps the elliptic.Curve interface to provide more convenient functionality.
 type ECWrapper struct {
 	curve elliptic.Curve
 }
 
+// NewECWrapper creates a new ECWrapper with the specified elliptic curve.
 func NewECWrapper(curve elliptic.Curve) *ECWrapper {
 	return &ECWrapper{
 		curve: curve,
 	}
 }
 
+// Params returns the parameters of the underlying elliptic curve.
 func (ec *ECWrapper) Params() *elliptic.CurveParams {
 	return ec.curve.Params()
 }
 
+// GetBasePointG returns the base point (generator) of the elliptic curve as an ECPoint.
 func (ec *ECWrapper) GetBasePointG() *ECPoint {
 	params := ec.Params()
 	Gx, Gy := params.Gx, params.Gy
 	return NewECPoint(Gx, Gy)
 }
 
+// IsOnCurve checks if the given ECPoint lies on the elliptic curve.
 func (ec *ECWrapper) IsOnCurve(point *ECPoint) bool {
 	params := ec.Params()
 	return params.IsOnCurve(point.Params())
 }
 
+// Add performs point addition on the elliptic curve and returns the result as an ECPoint.
 func (ec *ECWrapper) Add(point1, point2 *ECPoint) *ECPoint {
 	params := ec.Params()
 	x1, y1 := point1.Params()
@@ -37,11 +43,13 @@ func (ec *ECWrapper) Add(point1, point2 *ECPoint) *ECPoint {
 	return NewECPoint(params.Add(x1, y1, x2, y2))
 }
 
+// Double performs point doubling on the elliptic curve and returns the result as an ECPoint.
 func (ec *ECWrapper) Double(point *ECPoint) *ECPoint {
 	params := ec.Params()
 	return NewECPoint(params.Double(point.Params()))
 }
 
+// ScalarMult performs scalar multiplication of an ECPoint with a scalar value and returns the result as an ECPoint.
 func (ec *ECWrapper) ScalarMult(k *big.Int, point *ECPoint) *ECPoint {
 	params := ec.Params()
 	x, y := point.Params()
